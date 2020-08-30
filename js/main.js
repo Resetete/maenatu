f36b7580de9afb0773836f649c7bd2c4d0a8588d
Theresa <tmannschatz@gmail.com>
2020-08-30 21:41:57 +0200

    Add handwriting SVG, add background images

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ function myFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("headers").className = "headers-shown";
   } else {
-    document.getElementById("headers").className = "";
+    document.getElementById("headers").className = "headers-hidden";
   }
 }
+
+
+var wrapper = document.querySelector('.wrapper svg')
+
+// We are only adding 'active' class,
+// the entire animation is defined in the CSS code
+function draw() {
+  wrapper.classList.add('active')
+}
+
+// Add handlers to our buttons
+wrapper.addEventListener('onscroll', draw, true)
+
+// Play draw animation once
+setTimeout(draw, 600)
